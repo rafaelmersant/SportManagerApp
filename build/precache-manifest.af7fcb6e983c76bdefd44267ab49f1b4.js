0521cca32e60f66e7382614b2b73b6ca0e8c0e60
Rafael Mercedes <rafaelmersant@gmail.com>
2020-03-29 21:28:48 -0400

    Deploy updated

---
+++ build/precache-manifest.af7fcb6e983c76bdefd44267ab49f1b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927ced08da6078b773a9c5bbe7d9b3bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d09be00679e2e856532",
+    "url": "/static/css/2.c5334539.chunk.css"
+  },
+  {
+    "revision": "ea5cd5f9ee60b4933f7e",
+    "url": "/static/css/main.98fb2167.chunk.css"
+  },
+  {
+    "revision": "7d09be00679e2e856532",
+    "url": "/static/js/2.0768e6c6.chunk.js"
+  },
+  {
+    "revision": "8628dcdbe14732325e6394b61b4b6aec",
+    "url": "/static/js/2.0768e6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5cd5f9ee60b4933f7e",
+    "url": "/static/js/main.c9a3f835.chunk.js"
+  },
+  {
+    "revision": "e5a94623ba2182831df5",
+    "url": "/static/js/runtime-main.3e3de054.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
