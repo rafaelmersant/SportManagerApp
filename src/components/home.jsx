c9354cdb5c66deaab768fd0a8549ea65d9d8cfc2
Rafael Mercedes <rafaelmersant@gmail.com>
2020-03-29 21:39:29 -0400

    Deploy update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="text-center">
       <img
         style={{ width: "50%" }}
-        src={process.env.PUBLIC_URL + "/images/fenix.png"}
+        src="https://firebasestorage.googleapis.com/v0/b/fenix-system-8a811.appspot.com/o/photos%2Ffenix.png?alt=media&token=8cf3ea67-39be-4647-8b2e-88a9966805e6"
         alt="FENIX"
       />
     </div>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ user }) => {
       <NavLink className="navbar-brand" to="/">
         <img
           width="60px"
-          src={process.env.PUBLIC_URL + "/images/fenix.png"}
+          src="https://firebasestorage.googleapis.com/v0/b/fenix-system-8a811.appspot.com/o/photos%2Ffenix.png?alt=media&token=8cf3ea67-39be-4647-8b2e-88a9966805e6"
           alt="FENIX"
         />
       </NavLink>
