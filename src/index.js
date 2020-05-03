9f475b301b11bf28e628eee7ab36b3627a5871bc
Rafael Mercedes <rafaelmersant@gmail.com>
2020-05-03 19:26:26 -0400

    Uncomment jquery reference

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap";
 import "bootstrap/dist/js/bootstrap.js";
-// import $ from "jquery";
+import $ from "jquery";
 // import Popper from "popper.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
